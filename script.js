ee0e38ab05df33aca5f7903067e545049fe6bb63
lisps <stummp@loewen.de>
2014-02-16 22:50:45 +0100

    display also page title

--- script.js
+++ script.js
@@ -16,29 +16,31 @@ jQuery(function(){
                     console.log(data);
                     data=JSON.parse(data);
                     callback(jQuery.map(data.data,function(item){
-                        var res = item.id;
+                        var id = item.id;
                         if(data.linktype === 'absolute') 
-                            res = ':' + res;
+                            id = ':' + id;
                         if(item.type === 'd')
-                            res = res + ':';
+                            id = id + ':';
                         
-                        return res;
+                        return {id:id,title:item.title};
                     }));
                 }
             );
         },
         template:function(value){
             var image = '';
-            
+            var title = value.title?' ('+linksuggest_escape(value.title)+')':'';
+            value = value.id;
             if(value.slice(-1) === ':'){ //namespace
                 image = 'ns.png';
             } else { //file
                 image = 'fileicons/file.png';
             }
-            return '<img src="'+DOKU_BASE+'lib/images/'+image+'"> '+linksuggest_escape(value);
+            return '<img src="'+DOKU_BASE+'lib/images/'+image+'"> '+linksuggest_escape(value) + title;
         },
         index: 1,
         replace: function (element) {
+            element = element.id;
             if(element.slice(-1) === ':'){ //namespace
                 return '[[' + linksuggest_escape(element);
             } else { //file
